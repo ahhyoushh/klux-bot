ff494c220049c640dcc1b84bd1cd3543a356bd2c
Ayush32173 <bhaleraoayush06@gmail.com>
2022-03-29 21:28:51 +0530

    firs

--- Bot.py
+++ Bot.py
@@ -1,7 +1,4 @@
 
-from optparse import Option
-from secrets import choice
-from unicodedata import name
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
@@ -22,6 +19,33 @@ async def on_ready():
 )
 async def _ping(ctx):
     await ctx.send(f'The bots ping is {round(client.latency * 1000)}ms')
+
+@slash.slash(
+    name="Announce",
+    description="To announce something important",
+    guild_ids=[883746608905334835],
+    options=[
+        create_option(
+            name="title",
+            description="Title for the message",
+            required=True,
+            option_type=3
+        ),
+        create_option(
+            name="description",
+            description='Give the description for the Announcement',
+            required=True,
+            option_type=3
+        )
+
+    ]
+)
+async def _Announce(ctx, title:str, description:str):
+    embed = discord.Embed(title=title, description=description, color=discord.Color.red())
+    announcementicon = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fannouncement_1870127&psig=AOvVaw1CBmDqo9_Hf6o7vFXDwgR7&ust=1648654608469000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPC3gZbT6_YCFQAAAAAdAAAAABAD',
+    embed.set_thumbnail(url=announcementicon)
+    embed.set_footer(icon_url='https://img.icons8.com/ios-glyphs/344/verified-account--v1.png', text = "Posted By A certified Mod")
+    await ctx.send(embed)
 
 @slash.slash(
     name="lolxd",
@@ -71,4 +95,4 @@ async def _lolxd(ctx,  type:str, description:str, link:str):
     embed.set_footer(icon_url='https://img.icons8.com/ios-glyphs/344/verified-account--v1.png', text = "Posted By A certified Memer")
     await ctx.send(embed=embed)
 
-client.run('OTI0MTk0NDU3MDk4Nzg4ODk0.YcbBKQ.e7N07gyLidCNjzkdYqfLujgTET4')
+client.run('OTI0MTk0NDU3MDk4Nzg4ODk0.YcbBKQ.pa4C3eZP79QCEdVcQX08zrH4sOU')
